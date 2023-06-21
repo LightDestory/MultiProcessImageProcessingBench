2eca31ce7395fbe58f488035b713e038d550cbf6
LightDestory <apb231@gmail.com>
2023-06-21 12:13:04 +0200

    fix: multiprocessing on bundle configuration

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
+import multiprocessing
+import sys, os
 from ui.gui_interface import App
 
 if __name__ == "__main__":
+    if getattr(sys, 'frozen', False):
+        multiprocessing.freeze_support()
     app = App()
     try:
         app.mainloop()

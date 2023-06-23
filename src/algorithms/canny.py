6c6676a9d61ea39704d4ed1272675d1d46772eb5
LightDestory <apb231@gmail.com>
2023-06-23 13:34:09 +0200

    refactor/fix: Use native conv, fix non-square sub-images for canny

--- src/algorithms/canny.py
+++ src/algorithms/canny.py
@@ -1,97 +1,35 @@
 from PIL import Image
 import numpy as np
-import scipy.stats as st
-from math import atan
-from .convolution import convolution_kernels
+from .convolution import generate_gauss_kernel, convolve_native, convolution_kernels
 
 np.seterr(divide='ignore', invalid='ignore', over='ignore')
 name: str = "Canny Edge Detection"
 
 
-def _generate_gauss_kernel(kernel_length: int, sigma: int) -> np.ndarray:
+def _non_maximal_supress(gradient_magnitude: np.ndarray, gradient_direction: np.ndarray) -> np.ndarray:
     """
-    Generate a gauss kernel
-    :param kernel_length: The kernel length
-    :param sigma: The sigma value
-    :return: The gauss kernel, an np.ndarray
+    Suppresses the non-maximal values in the given gradient magnitude array.
+    :param gradient_magnitude: The gradient magnitude array.
+    :param gradient_direction: The gradient direction array.
+    :return: The suppressed gradient magnitude array.
     """
-    interval: float = (2 * sigma + 1.) / kernel_length
-    x = np.linspace(-sigma - interval / 2., sigma + interval / 2., kernel_length + 1)
-    kern1d = np.diff(st.norm.cdf(x))
-    kernel_raw = np.sqrt(np.outer(kern1d, kern1d))
-    kernel = kernel_raw / kernel_raw.sum()
-    return kernel
-
-
-def _apply_kernel_to_array(image_array: np.ndarray, kernel: np.ndarray, kernel_size: int) -> np.ndarray:
-    """
-    Applies the given kernel to the given image array.
-    :param image_array: An np.ndarray representing the image.
-    :param kernel: The kernel to apply.
-    :param kernel_size: The size of the kernel.
-    :return: The image array with the kernel applied.
-    """
-    width: int = len(image_array[0])
-    height: int = len(image_array)
-    result = np.empty([height - 2 * kernel_size, width - 2 * kernel_size])
-    for x in range(kernel_size, width - 2 * kernel_size):
-        for y in range(kernel_size, height - 2 * kernel_size):
-            window = image_array[y - kernel_size:y + kernel_size + 1, x - kernel_size:x + kernel_size + 1]
-            acc: int = 0
-            for kx in range(0, 2 * kernel_size + 1):
-                for ky in range(0, 2 * kernel_size + 1):
-                    acc += kernel[kx][ky] * window[kx][ky]
-            result[y][x] = max(0, min(255, int(acc)))
-    return result
-
-
-def _calc_magnitude_and_degree(sobel_x: list[list[int]], sobel_y: list[list[int]]) -> (np.ndarray, np.ndarray):
-    """
-    Calculates the magnitude and degree of the given sobel x and sobel y arrays.
-    :param sobel_x: The sobel x array.
-    :param sobel_y: The sobel y array.
-    :return: The magnitude and degree arrays in a tuple.
-    """
-    width: int = len(sobel_x[0])
-    height: int = len(sobel_x)
-    mag: np.ndarray = np.empty([height, width])
-    degree: np.ndarray = np.empty([height, width])
-    for x in range(0, width):
-        for y in range(0, height):
-            mag[y][x] = np.math.sqrt(sobel_x[y][x] * sobel_x[y][x] + sobel_y[y][x] * sobel_y[y][x])
-            degree[y][x] = atan(sobel_y[y][x] / sobel_x[y][x])
-    degree = (np.round(degree * (5.0 / np.pi)) + 5) % 5
-    return mag, degree
-
-
-def _non_maximal_supress(image_array: np.ndarray, grad_deg: np.ndarray) -> np.ndarray:
-    """
-    Performs non-maximal suppression on the given image array.
-    :param image_array: The image array.
-    :param grad_deg: The gradient degree array.
-    :return: The image array with non-maximal suppression applied.
-    """
-    width: int = len(image_array[0])
-    height: int = len(image_array)
-    for x in range(0, width):
-        for y in range(0, height):
-            if x == 0 or y == height - 1 or y == 0 or x == width - 1:
-                image_array[y][x] = 0
-                continue
-            direction: int = grad_deg[y][x] % 4
-            if direction == 0:
-                if image_array[y][x] <= image_array[y][x - 1] or image_array[y][x] <= image_array[y][x + 1]:
-                    image_array[y][x] = 0
-            if direction == 1:
-                if image_array[y][x] <= image_array[y - 1][x + 1] or image_array[y][x] <= image_array[y + 1][x - 1]:
-                    image_array[y][x] = 0
-            if direction == 2:
-                if image_array[y][x] <= image_array[y - 1][x] or image_array[y][x] <= image_array[y + 1][x]:
-                    image_array[y][x] = 0
-            if direction == 3:
-                if image_array[y][x] <= image_array[y - 1][x - 1] or image_array[y][x] <= image_array[y + 1][x + 1]:
-                    image_array[y][x] = 0
-    return image_array
+    suppressed_magnitude = np.zeros_like(gradient_magnitude)
+    angles = np.rad2deg(gradient_direction)
+    angles[angles < 0] += 180
+    for i in range(1, gradient_magnitude.shape[0] - 1):
+        for j in range(1, gradient_magnitude.shape[1] - 1):
+            direction = angles[i, j]
+            if (0 <= direction < 22.5) or (157.5 <= direction <= 180):  # 0 degrees
+                neighbors = [gradient_magnitude[i, j - 1], gradient_magnitude[i, j + 1]]
+            elif 22.5 <= direction < 67.5:  # 45 degrees
+                neighbors = [gradient_magnitude[i - 1, j - 1], gradient_magnitude[i + 1, j + 1]]
+            elif 67.5 <= direction < 112.5:  # 90 degrees
+                neighbors = [gradient_magnitude[i - 1, j], gradient_magnitude[i + 1, j]]
+            else:  # 135 degrees
+                neighbors = [gradient_magnitude[i - 1, j + 1], gradient_magnitude[i + 1, j - 1]]
+            if gradient_magnitude[i, j] >= max(neighbors):
+                suppressed_magnitude[i, j] = gradient_magnitude[i, j]
+    return suppressed_magnitude
 
 
 def _double_threshold(image_array: np.ndarray, low_threshold: int, high_threshold: int) -> np.ndarray:
@@ -102,10 +40,10 @@ def _double_threshold(image_array: np.ndarray, low_threshold: int, high_threshol
     :param high_threshold:
     :return: The filtered image array.
     """
-    image_array[np.where(image_array > high_threshold)] = 255
-    image_array[np.where((image_array >= low_threshold) & (image_array <= high_threshold))] = 75
-    image_array[np.where(image_array < low_threshold)] = 0
-    return image_array
+    edge_map = np.zeros_like(image_array)
+    edge_map[(image_array >= high_threshold)] = 255
+    edge_map[(image_array >= low_threshold) & (image_array < high_threshold)] = 127
+    return edge_map
 
 
 def _link_edges(image_array: np.ndarray) -> np.ndarray:
@@ -114,35 +52,38 @@ def _link_edges(image_array: np.ndarray) -> np.ndarray:
     :param image_array: The image array.
     :return: Tge image array with the edges linked.
     """
-    width: int = len(image_array[0])
-    height: int = len(image_array)
-    for i in range(0, height):
-        for j in range(0, width):
-            if image_array[i][j] == 75:
-                if ((image_array[i + 1][j] == 255) or (image_array[i - 1][j] == 255) or (
-                        image_array[i][j + 1] == 255) or (
-                        image_array[i][j - 1] == 255) or (image_array[i + 1][j + 1] == 255) or (
-                        image_array[i - 1][j - 1] == 255)):
-                    image_array[i][j] = 255
+    for i in range(1, image_array.shape[0] - 1):
+        for j in range(1, image_array.shape[1] - 1):
+            if image_array[i, j] == 127:
+                if np.max(image_array[i - 1:i + 2, j - 1:j + 2]) == 255:
+                    image_array[i, j] = 255
                 else:
-                    image_array[i][j] = 0
+                    image_array[i, j] = 0
     return image_array
 
 
 def canny_edge_detector(target_image: Image.Image, gauss_size: int, sigma: int, low_threshold: int,
-                        high_threshold: int):
-    pixel = np.array(target_image.convert("L"))
-    result_array = pixel.copy()
+                        high_threshold: int) -> Image.Image:
+    """
+    Applies the canny edge detection algorithm to the given image.
+    :param target_image: The image to apply the canny algorithm to.
+    :param gauss_size: The size of the gaussian kernel.
+    :param sigma: The sigma value for the gaussian kernel.
+    :param low_threshold: The low threshold for the double thresholding.
+    :param high_threshold: The high threshold for the double thresholding.
+    :return: The image with the canny algorithm applied.
+    """
+    target_image = target_image.convert("L")
     # Phase 1 Smoothing applying gaussian kernel
-    gauss_kernel = _generate_gauss_kernel(2 * gauss_size + 1, sigma)
-    result_array = _apply_kernel_to_array(result_array, gauss_kernel, gauss_size)
-    result_array = result_array.astype(np.uint8)
+    gauss_kernel = generate_gauss_kernel(gauss_size, sigma)
+    gauss_image = convolve_native(target_image, gauss_kernel)
     # Phase 2 Finding the gradients
-    sobel_x_image = _apply_kernel_to_array(result_array, np.array(convolution_kernels["edge_detection (sobel-x)"]), 1)
-    sobel_y_image = _apply_kernel_to_array(result_array, np.array(convolution_kernels["edge_detection (sobel-y)"]), 1)
-    grad_mag, grad_deg = _calc_magnitude_and_degree(sobel_x_image, sobel_y_image)
+    sobel_x_array = np.array(convolve_native(gauss_image, convolution_kernels["edge_detection (sobel-x)"], True))
+    sobel_y_array = np.array(convolve_native(gauss_image, convolution_kernels["edge_detection (sobel-y)"], True))
+    gradient_magnitude = np.hypot(sobel_x_array, sobel_y_array)
+    gradient_direction = np.arctan2(sobel_y_array, sobel_x_array)
     # Phase 3 Non-maximal supression
-    supressed = _non_maximal_supress(grad_mag, grad_deg)
+    supressed = _non_maximal_supress(gradient_magnitude, gradient_direction)
     # Phase 4 Double thresholding
     thresholded = _double_threshold(supressed, low_threshold, high_threshold)
     # Phase 5 Edge tracking by linking edges
